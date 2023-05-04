585d23cfe200ba9de32f941fdafe1bb1df7e6ada
brkidder <r.ben.kidder@gmail.com>
2023-05-03 21:16:58 -0500

    Set up and Resize app window

--- main.py
+++ main.py
@@ -1,39 +1,21 @@
-# Import Formulas by Method Names
-import sys
-from formulas import add, subtract, multiply, divide
+from view import *
 
 
 # Run Program
 def main():
-    # Retrieve Argument List at Program Init
-    term_input = sys.argv
+    window = Tk()
+    window.title('A Better Calculator')
+    width = 400
+    height = 500
+    screen_width = window.winfo_screenwidth()
+    screen_height = window.winfo_screenheight()
+    x_cord = int((screen_width / 2) - (width / 2))
+    y_cord = int((screen_height / 2) - (height / 2))
+    window.geometry(f'{width}x{height}+{x_cord}+{y_cord}')
+    window.resizable(False, False)
 
-    # Exit with Invalid Argument Length
-    if len(term_input) <= 1:
-        sys.exit("Need to provide the operator name")
-    elif len(term_input) <= 3:
-        sys.exit("Need to provide at least two values")
-
-    # Define Portions of Argument List
-    operator = term_input[1]
-    values = [float(val) for val in term_input[2:]]
-    # Init Final Answer
-    answer = 0
-
-    # Operator Check and Execution
-    if operator not in ["add", "subtract", "multiply", "divide"]:
-        sys.exit("Valid operator names (add, subtract, multiply, divide)")
-    elif operator == "add":
-        answer = add(values)
-    elif operator == "subtract":
-        answer = subtract(values)
-    elif operator == "multiply":
-        answer = multiply(values)
-    elif operator == "divide":
-        answer = divide(values)
-
-    # Print Final Answer
-    print(f'Answer = {answer:.2f}')
+    GUI(window)
+    window.mainloop()
 
 
 # Init Program
